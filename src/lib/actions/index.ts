5ac5ce4b789f265e1c34e3026642b4bf57cb30ad
who-shubham <shubham.2022ug2012@iiitranchi.ac.in>
2026-02-08 01:53:48 +0530

    feat: add server actions index

---
+++ src/lib/actions/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Server actions - use from client components or stores.
+ * No tRPC; all APIs are server actions.
+ */
+
+export * from './workflow';
+export * from './folder';
+export * from './history';
+export * from './llm';
